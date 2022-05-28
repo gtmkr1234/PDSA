c3f4cfe45d88eb0e436707213fe039e1ebc7944b
pc <gtmkr1234@gmail.com>
2022-05-28 16:13:42 +0530

    calculate gcd of two numbers

--- gcd_bruteforce.py
+++ gcd_bruteforce.py
@@ -1,8 +1,9 @@
-def gcd(m,n):
-    for i in range(1,min(m,n)+1):
+def gcd(m, n):
+    for i in range(1, min(m, n) + 1):
         if (m % i == 0) and (n % i == 0):
             temp = i
     return temp
 
+
 if __name__ == '__main__':
-    print(gcd(15454,6524))
+    print(gcd(15454, 6524))
