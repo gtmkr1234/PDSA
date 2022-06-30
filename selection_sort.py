b479b496544de2cb06d752f692228b2ddbc2ae37
pc <gtmkr1234@gmail.com>
2022-06-30 22:57:59 +0530

    selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -3,11 +3,14 @@ def sel_sort(L):
     if (n < 1):
         return L
     for i in range(n):
+        #Assuming that L[:i] is sorted
         mpos = i
         for j in range(i + 1, n):
             if L[j] < L[mpos]:
                 mpos = j
+                #position of minimum element in L
         (L[i], L[mpos]) = (L[mpos], L[i])
+        # L[n+i:] is sorted
     return (L)
 
 
