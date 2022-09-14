c5d1c498ee9fcc47ec1d06117eafc8a33771c733
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-15 01:26:26 +0530

    Python program to chack various properties of triangle by using classes and objects

--- Grpa_and_Ppa_Sol/Week1/ppa2.py
+++ Grpa_and_Ppa_Sol/Week1/ppa2.py
@@ -1,9 +1,43 @@
+import math
+
+
 class Triangle:
     def __init__(self, x, y, z):
         self.a = x
         self.b = y
-        self.z = z
+        self.c = z
 
+    def Is_valid(self):
+        if ((self.a + self.b) > self.c) and ((self.b + self.c) > self.a) and ((self.a + self.c) > self.b):
+            return "Valid"
+        else:
+            return "Invalid"
+
+    def Side_Classification(self):
+        if self.Is_valid() != "Invalid":
+            if self.a == self.b == self.c:
+                return "Equilateral"
+            if self.a == self.b or self.b == self.c or self.a == self.c:
+                return "Isosceles"
+            else:
+                return "Scalene"
+        return "Invalid"
+
+    def Angle_Classification(self):
+        if self.Is_valid() != "Invalid":
+            if self.a ** 2 + self.b ** 2 > self.c ** 2:
+                return "Acute"
+            if self.a ** 2 + self.b ** 2 < self.c ** 2:
+                return "Obtuse"
+            if self.a ** 2 + self.b ** 2 == self.c ** 2:
+                return "Right"
+        return "Invalid"
+
+    def Area(self):
+        if self.Is_valid() != "Invalid":
+            s = (self.a+self.b+self.c)/2
+            return math.sqrt(s*(s-self.a)*(s-self.b)*(s-self.c))
+        return "Invalid"
 
 if __name__ == '__main__':
     a = int(input())
