39de27d52cfc631e5178028e4babf6de5788178b
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-14 13:28:58 +0530

    MergeSort Implementation with calculating the time by making a Timer class

--- gcd.py
+++ gcd.py
@@ -1,9 +1,18 @@
-def gcd(m,n):
-    (a,b) = (max(m,n), min(m,n))
-    if (a%b==0):
+def gcd(m, n):
+    (a, b) = (max(m, n), min(m, n))
+    if a % b == 0:
         return b
     else:
-        return gcd(b,(a-b))
+        return gcd(b, (a - b))
+
+
+def gcd_euclid(m, n):
+    (a, b) = (max(m, n), min(m, n))
+    if a % b == 0:
+        return b
+    else:
+        return gcd_euclid(b, a % b)
+
 
 if __name__ == "__main__":
-    print(gcd(95,2))
+    print(gcd_euclid(95, 2))
