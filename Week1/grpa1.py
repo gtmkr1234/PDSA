efc8f53a6cd9d10365fba1941a3ef4701f636893
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-15 00:13:48 +0530

    MergeSort Implementation with calculating the time by making a Timer class

---
+++ Week1/grpa1.py
@@ -0,0 +1,7 @@
+def find_Min_Difference(L,P):
+
+
+if __name__ == '__main__':
+    L=eval(input().strip())
+    P=int(input())
+    print(find_Min_Difference(L,P))
---
+++ test2.py
@@ -0,0 +1,6 @@
+x=1
+while True:
+    if x % 5 == 0:
+        break
+    print(x, end=" ")
+    x+=1
