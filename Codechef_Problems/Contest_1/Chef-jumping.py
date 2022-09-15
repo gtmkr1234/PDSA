723f7a1f52c267b588f8c3bbbd5a1a8ac71f8e85
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-16 00:24:31 +0530

    earlier algorithm was slow.. Was throwing TLE

--- Codechef_Problems/Contest_1/Chef-jumping.py
+++ Codechef_Problems/Contest_1/Chef-jumping.py
@@ -1,18 +1,8 @@
 def canJump(n):
     true_stmt = "yes"
     false_stmt = "no"
-    if n == 0:
+    if n%3==0 or n%6==1:
         return true_stmt
-    while n > 0:
-        n -= 1
-        if n == 0:
-            return true_stmt
-        n -= 2
-        if n == 0:
-            return true_stmt
-        n -= 3
-        if n == 0:
-            return true_stmt
     return false_stmt
 
 
