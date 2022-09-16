979138ef1d671b6d108531912dc9c62d56970332
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-16 09:26:11 +0530

    solution for grpa1.py

--- Grpa_and_Ppa_Sol/Week1/grpa1.py
+++ Grpa_and_Ppa_Sol/Week1/grpa1.py
@@ -1,7 +1,16 @@
-def find_Min_Difference(L,P):
-    pass
+import sys
+
+
+def find_Min_Difference(L, P):
+    L.sort()
+    min_diff = sys.maxsize
+    n = len(L)
+    for i in range(n - P + 1):
+        min_diff = min(min_diff, abs(L[i] - L[i + P - 1]))
+    return min_diff
+
 
 if __name__ == '__main__':
-    L=eval(input().strip())
-    P=int(input())
-    print(find_Min_Difference(L,P))
+    L = eval(input().strip())
+    P = int(input())
+    print(find_Min_Difference(L, P))
