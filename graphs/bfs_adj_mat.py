ae17b671bdf0ff82cd2f9958d30cdfea744b08e2
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-11-09 01:14:44 +0530

    Queue

---
+++ graphs/bfs_adj_mat.py
@@ -0,0 +1,40 @@
+from Queue.Queue import Queue
+import numpy as np
+
+
+def bfsAdjMat(AdjMat, vertex):
+    (rows, cols) = AdjMat.shape
+    visited = {}
+    for start in range(rows):
+        visited[start] = False
+    q = Queue()
+
+    visited[vertex] = True
+    q.addq(vertex)
+
+    while not q.isEmpty():
+        temp = q.delq()
+        for k in neighbours(AdjMat, temp):
+            if not visited[k]:
+                visited[k] = True
+                q.addq(k)
+
+    return visited
+
+
+def neighbours(AdjMat, ver):
+    res = []
+    (row, col) = AdjMat.shape
+    for j in range(col):
+        if AdjMat[ver][j] == 1:
+            res.append(j)
+    return res
+
+
+if __name__ == '__main__':
+    edge_list = [(0, 1), (1, 2), (2, 3), (0, 2), (3, 2), (4, 5), (5, 4), (1, 4)]
+    mat = np.zeros(shape=(10, 10))
+    for (i, j) in edge_list:
+        mat[i, j] = 1
+
+    print(bfsAdjMat(mat, 1))
