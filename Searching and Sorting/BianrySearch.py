b200e911ca7d0fed28c6ddc02d04e4668101e687
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-11-08 19:02:53 +0530

    Queue

---
+++ Searching and Sorting/BianrySearch.py
@@ -0,0 +1,17 @@
+def binarySearch(lst: list[int], val: int) -> bool:
+    start = 0
+    end = len(lst)-1
+
+    while end-start > 1:
+        mid = (start + end) // 2
+        if lst[mid] == val:
+            return True
+        if lst[mid] > val:
+            end = mid-1
+        if lst[mid] < val:
+            start = mid+1
+    if lst[start]==val or lst[end] == k:
+        return True
+    return False
+
+
