5a4dd7da386f7b32e1229c290c2d69f449b3cf20
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-11-09 13:14:17 +0530

    Queue

--- graphs/bfsClass.py
+++ graphs/bfsClass.py
@@ -1,10 +1,10 @@
 from queue import Queue
 
-adj_lst = {"A":["B","C"], "B":["D","E"], "C":["B","F"], "E":["F"]}
+adj_lst = {"A": ["B", "C"], "B": ["D", "E"], "C": ["F", "G"], "D": ["H", "I"], "E": ["J", "K"], "F": ["L", "M"], "G": ["N", "O"]}
 visited = {}
 level = {}
 output = []
-for i in ["A","B","C","D","E","F"]:
+for i in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"]:
     level[i] = -1
     visited[i] = False
 s = "A" #Starting Index
@@ -17,7 +17,7 @@ while not queue.empty():
     output.append(u)
     if u in adj_lst.keys():
         for v in adj_lst[u]:
-            if visited[v] == False:
+            if not visited[v]:
                 visited[v] = True
                 queue.put(v)
                 level[v] = level[u] + 1
